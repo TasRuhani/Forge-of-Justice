4bdf5d1db8b5c9b498bf7c453fb03c53bcced2f3
TasRuhani <tasdeequerockz@gmail.com>
2025-06-20 19:14:23 +0530

    import problem fix in inngest.js

--- config/inngest.js
+++ config/inngest.js
@@ -1,7 +1,7 @@
 import { Inngest } from "inngest";
 import { connect } from "mongoose";
 import ConnectDB from "./db";
-import User from "./models/User";
+import User from "../models/User";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "ecom-next" });
