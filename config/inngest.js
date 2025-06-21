bffd6afdd1981fb29aa27e30450f9189e045216f
TasRuhani <tasdeequerockz@gmail.com>
2025-06-21 12:32:54 +0530

    cartitems default{}  added to user creation

--- config/inngest.js
+++ config/inngest.js
@@ -21,6 +21,7 @@ export const syncUserCreation = inngest.createFunction(
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             imageUrl: image_url,
+            cartItems: {}
         }
         await ConnectDB();
         await User.create(userData)
